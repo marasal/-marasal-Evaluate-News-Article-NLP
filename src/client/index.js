d699d5a1829809b2eec709ae704823d1d7d429c8
marasal <mar.603@hotmail.com>
2021-02-05 05:42:33 +0300

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,21 @@
+//import functional files
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+//import style files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+//console.log(checkForName);
+
+//alert("I EXIST")
+//console.log("CHANGE!!");
+
+
+// export into client library
+export {
+   checkForName,
+   handleSubmit
+  }
